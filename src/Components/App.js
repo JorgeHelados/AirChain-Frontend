5191d328b20cc448ff667790e3165f09cdbbff43
Jorge Helados <jorgesatorres007@gmail.com>
2024-10-18 17:14:34 +0200

    Añadimos a John React
    Ahora tenemos el frontend

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+import Body from './body';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header/> */}
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/body.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [ultimoValor, setUltimoValor] = useState('');
+
+    useEffect(() => {
+        const fetchData = () => {
+            fetch('http://localhost:4000/api/medicion') // Enlace de localhost con la conexión
+                .then(response => response.json())
+                .then(data => setUltimoValor(data.cantidad))
+                .catch(error => console.error('Error fetching data:', error));
+        };
+
+        fetchData();
+        const interval = setInterval(fetchData, 1000); // Fetch data every second
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center'
+        }}>
+            <h1 style={{ fontSize: '4rem' }}>
+                Último valor: {ultimoValor}
+            </h1>
+        </div>
+    );
+}
+
+export default App;
