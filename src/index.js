5be307be1318eed3bf0c3b6810c307ed4c86db5a
Jorge Helados <jorgesatorres007@gmail.com>
2024-10-21 19:16:26 +0200

    Añadida la pagina de contacto y su CSS
    Ya esta la pagina de contacto y su CSS

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Login from './Components/login';
 import Register from './Components/register';
 import Terminos from './Components/terminos';
 import AcercaDe from './Components/acerca_de';
+import Contacto from './Components/contacto';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,13 +23,11 @@ root.render(
         <Route path="/register" element={<Register />} />
         <Route path="/terminos" element={<Terminos />} />
         <Route path="/acerca_de" element={<AcercaDe />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
       <Footer />
     </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
