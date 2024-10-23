deb00b99f8c698403d34a18e82531a7560f09c02
Emilio-sg13 <esangra@epsg.upv.es>
2024-10-23 19:48:17 +0200

    funcionalidad-InicioSesion

---
+++ src/js/inicio.js
@@ -0,0 +1,21 @@
+const apiBaseUrl = 'http://192.168.1.28:4000/';
+
+// Función para iniciar sesión
+export async function iniciarSesion(nombre, contrasenya) {
+    try {
+        const response = await fetch(`${apiBaseUrl}api/gases/usuarios?Correo=${encodeURIComponent(nombre)}&Contrasenya=${encodeURIComponent(contrasenya)}`, {
+            method: 'GET',
+        });
+        if (!response.ok) {
+            throw new Error('Error en el inicio de sesión');
+        }
+        const data = await response.json();
+        console.log('Sesión iniciada:', data);
+        // Retornar los datos del usuario
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        console.log('aaaaaaaaaaa');
+        throw error; // Lanzar el error para manejarlo más adelante
+    }
+}
