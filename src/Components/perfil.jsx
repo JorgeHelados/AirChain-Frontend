18b602b2b448701440a3cc0d3ea1c277a209ccbd
Jorge Helados <jorgesatorres007@gmail.com>
2024-10-24 19:34:53 +0200

    Vuelta ha hacer el editar prefil bien hecho
    Lo siento Emilio por lo de login y register

--- src/Components/perfil.jsx
+++ src/Components/perfil.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
-import '../Style/formularios.css';
+import '../Style/perfil.css';
 
 function Perfil() {
     return (
-    <div className="profile-container">
-        <div className="profile-box">
-            <h2 className='h2-form'>Mi perfil</h2>
-            <div className='columns-box'>
-                <div className='column'>
-                    <input type="name" placeholder="Nombre" className='input-form'/>
-                    <input type="surname" placeholder="Apellido" className='input-form'/>
-                    <button className='button-form'>Editar perfil</button>
+        <div className="perfil-page profile-container">
+            <div className="profile-box">
+                <h2 className='h2-perfil'>Mi perfil</h2>
+                <div className='columns-box'>
+                    <div className='column-perfil'>
+                        <p className='p-perfil'>¿Has cambiado de nombre o apellidos?</p>
+                        <input type="text" placeholder="Nombre" className='perfil-input'/>
+                        <input type="text" placeholder="Apellidos" className='perfil-input'/>
+                    </div>
+                    <div className='column-perfil'>
+                        <button className='button-perfil'>Editar nombre</button>
+                    </div>
                 </div>
-                <div className='column'>
-                    <input type="tel" placeholder="Número de teléfono" className='input-form'/>
-                    <input type="password" placeholder="Contraseña" className='input-form'/>
-                    <button className='button-error'>Cerrar sesión</button>
+
+                <div className='columns-box'>
+                    <div className='column-perfil'>
+                        <p className='p-perfil'>¿Has cambiado de número de teléfono?</p>
+                        <input type="tel" placeholder="Numero de teléfono" className='perfil-input'/>
+                    </div>
+                    <div className='column-perfil'>
+                        <button className='button-perfil'>Editar número de teléfono</button>
+                    </div>
+                </div>
+
+                <div className='columns-box'>
+                    <div className='column-perfil'>
+                        <p className='p-perfil'>Escriba primero su contraseña actual y posteriormente su nueva contraseña</p>
+                        <input type="password" placeholder="Contraseña Actual" className='perfil-input'/>
+                        <input type="password" placeholder="Contraseña Nueva" className='perfil-input'/>
+                    </div>
+                    <div className='column-perfil'>
+                        <button className='button-perfil'>Editar contraseña</button>
+                    </div>
+                </div>
+
+                <div className='columns-box'>
+                    <div className='column-perfil'>
+                        <button className='button-logout'>Cerrar sesión</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 }
 
