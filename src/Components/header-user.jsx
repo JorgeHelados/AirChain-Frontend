e6b55dffa9074cb47f372a62e63fa05f18e4b835
Jorge Helados <jorgesatorres007@gmail.com>
2024-11-05 14:20:41 +0100

    Ahora se puede ir del perfil a la pagina con las mediciones

--- src/Components/header-user.jsx
+++ src/Components/header-user.jsx
@@ -13,7 +13,7 @@ const Header_User = () => {
       <nav>
         <ul className="nav-links">
           <li><a href="/">Inicio</a></li>
-          <li><a href="/acerca_de">Acerca de</a></li>
+          <li><a href="/user">Mis mediciones</a></li>
           <li><a href="/contacto">Contacto</a></li>
           <li>
             <a className="login-button" href="/perfil">
